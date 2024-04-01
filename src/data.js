d3865be0b8c5da028b9862b8a4e7e12478d45945
karthikeya <154613282+karthikeya360@users.noreply.github.com>
2024-04-01 08:59:04 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,52 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "images/No-Image-Placeholder.jpg"
+}, {    
+    "id": 1,
+    "title": "SCORPIO-N",
+    "price": 1360199,
+    "description": "SCORPIO-N is a sophisticated nanosatellite developed by NASA Ames Research Center. It's equipped with advanced imaging technology for Earth observation and scientific research. Its compact size enables cost-effective deployment and versatile applications in space exploration.",
+    "brand": "mahindra",
+    "image": "images/SCORPIO-N.jpg"
+}, {    
+    "id": 2,
+    "title": "XUV 700",
+    "price": 1399000,
+    "description": "The XUV700 is a flagship SUV manufactured by Mahindra & Mahindra, renowned for its cutting-edge features and advanced technology. It boasts luxurious interiors, powerful engine options, and a host of safety features, making it a popular choice in the premium SUV segment. With its bold design and impressive performance, the XUV700 competes with other top-tier SUVs in the market.",
+    "brand": "mahindra",
+    "image": "images/XUV 700.jpg"
+    }, {
+        "id": 3,
+        "title": "THAR",
+        "price": 1125000,
+        "description": "The Mahindra Thar is a rugged and versatile off-road SUV known for its iconic design and robust performance, suitable for both urban adventures and rough terrains. With its powerful engine options and enhanced capabilities, the Thar offers an exhilarating driving experience synonymous with exploration and adventure.",
+        "brand": "mahindra",
+        "image": "images/thar.jpg"
+}, {
+    "id": 4,
+    "title": "Punch",
+    "price": 1125000,
+    "description": "The Tata Punch is a compact SUV featuring a bold design and advanced features, offering a blend of style and performance for urban adventures. With its compact dimensions and agile handling, the Punch is designed to navigate city streets with ease while providing comfort and convenience for its occupants.",
+    "brand": "TATA",
+    "image": "images/Punch.jpg"
+    }, 
+    
+{
+   "id": 5,
+   "title": "Nexon",
+   "price": 1025000,
+   "description": "The Tata Nexon is a stylish and compact SUV known for its modern design, advanced features, and efficient performance, offering a comfortable and dynamic driving experience. With its sleek exterior and spacious interior, the Nexon combines versatility and innovation, making it a popular choice in its segment.",
+   "brand": "TATA",
+   "image": "images/Nexon.jpg"
+   }, {
+    "id": 6,
+"title": "Safari",
+"price": 1225000,
+"description": "The Tata Safari is a rugged and spacious SUV renowned for its commanding presence on and off the road, offering ample seating and versatile utility for adventurous journeys. With its bold design and capable performance, the Safari embodies the essence of exploration and comfort in the realm of Indian automotive excellence.",
+"brand": "TATA",
+"image": "images/Safari.jpg"
+}]
